6dc9d1098dcc5eaeb4d6ff9e86097b077859aea9
Angela-GM <angelagm0112@gmail.com>
2023-05-18 12:13:07 +0200

    complete

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,20 +85,132 @@ name: Patio
 */
 
 // Characters Collection
-const mrsPeacock = {name: "Peacko", edad:33}
+const mrGreen = {
+  first_name: "Jacob",
+  last_name: "Green",
+  color: "green",
+  description: "He has a lot of connections",
+  age: 45,
+  image:
+    "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  occupation: "Entrepreneur",
+};
+const drOrchid = {
+  first_name: "Doctor",
+  last_name: "Orchid",
+  color: "white",
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  occupation: "Scientist",
+};
+const profPlum = {
+  first_name: "Victor",
+  last_name: "Plum",
+  color: "purple",
+  description: "Billionare video game designer",
+  age: 22,
+  image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+  occupation: "Designer",
+};
+const missScarlet = {
+  first_name: "Kasandra",
+  last_name: "Scarlet",
+  color: "red",
+  description: "She is an A-list movie star with a dark past",
+  age: 31,
+  image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+  occupation: "Actor",
+};
+const mrsPeacock = {
+  first_name: "Eleanor",
+  last_name: "Peacock",
+  color: "blue",
+  description:
+    "She is from a wealthy family and uses her status and money to earn popularity",
+  age: 36,
+  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  occupation: "Socialité",
+};
+const mrMustard = {
+  first_name: "Jack",
+  last_name: "Mustard",
+  color: "yellow",
+  description:
+    "He is a former football player who tries to get by on his former glory",
+  age: 62,
+  image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+  occupation: "Retired Football player",
+};
 const charactersArray = [
-    {name: "Peacko", edad:33},
-
+  mrGreen,
+  drOrchid,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  mrMustard,
 ];
 
-charactersArray[0].name
-
+charactersArray[0].first_name;
 
 // Rooms' Collection
-const roomsArray = [{name:"ddd"}];
+const roomsArray = [
+  { name: "Dining Room" },
+  { name: "Conservatory" },
+  { name: "Kitchen" },
+  { name: "Study" },
+  { name: "Library" },
+  { name: "Billiard Room" },
+  { name: "Lounge" },
+  { name: "Ballroom" },
+  { name: "Hall" },
+  { name: "Spa" },
+  { name: "Living Room" },
+  { name: "Observatory" },
+  { name: "Theater" },
+  { name: "Guest House" },
+  { name: "Patio" },
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+  { name: "rope", weight: 10 },
+  { name: "knife", weight: 8 },
+  { name: "candlestick", weight: 2 },
+  { name: "dumbbell", weight: 30 },
+  { name: "poison", weight: 2 },
+  { name: "axe", weight: 15 },
+  { name: "bat", weight: 13 },
+  { name: "trophy", weight: 25 },
+  { name: "pistol", weight: 20 },
+];
 
+// Find a random element of the array - randomSelector › Defines randomSelector
+const randomSelector = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * array.length);
 
+  return array[randomIndex];
+};
 
+// Find a random element of the array - randomSelector › Return the element of a single value array
+
+const pickMistery = () => {
+  const mistery = [];
+  const singleCharacter = randomSelector(charactersArray);
+  const singleRoom = randomSelector(roomsArray);
+  const singleWeapon = randomSelector(weaponsArray);
+  mistery.push(singleCharacter);
+  mistery.push(singleWeapon);
+  mistery.push(singleRoom);
+  console.log(mistery);
+  return mistery;
+};
+
+pickMistery();
+
+const revealMistery = (mistery) => {
+  return `${mistery[0].first_name} ${mistery[0].last_name} killed Mr.Boddy using the ${mistery[1].name} in the ${mistery[2].name}!!!!`;
+};
